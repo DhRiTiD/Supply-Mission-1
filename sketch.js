be2044467247753d4bb5d70a7de4dd109734b272
Dhriti Dhall <74966124+DhRiTiD@users.noreply.github.com>
2021-01-01 18:33:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,26 +7,24 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
 }
 
 function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
-	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor = "green";
-
 
 	engine = Engine.create();
 	world = engine.world;
@@ -34,24 +32,20 @@ function setup() {
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.2, isStatic:true});
 	World.add(world, packageBody);
 	
-
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-
-
+	World.add(world, ground);
+	
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.x= packageBody.position.x; 
+  packageSprite.y= packageBody.position.y; 
   drawSprites();
- 
 }
 
 function keyPressed() {
@@ -59,6 +53,3 @@ function keyPressed() {
     Matter.Body.setStatic(packageBody, false);
   }
 }
-
-
-
